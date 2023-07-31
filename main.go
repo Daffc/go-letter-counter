da4a174a89ee5e9b2f44159c19282eaebfb4f72c
Douglas A.C <douglas.affc@gmail.com>
2023-07-31 17:36:43 -0300

    Reading content from input and writing into output.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	utils "github.com/Daffc/go-letter-counter/package/utils"
@@ -11,6 +12,24 @@ func main() {
 	nGoroutines, bufferSize, fInput, fOutput := utils.HandleArguemnts(os.Args[1:])
 	defer fInput.Close()
 	defer fOutput.Close()
+
+	buffer := make([]byte, bufferSize)
+
+	for {
+		readBytes, err := fInput.Read(buffer)
+
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
+			}
+
+			break
+		}
+
+		for i := 0; i < readBytes; i++ {
+			fmt.Fprint(fOutput, string(buffer[i]))
+		}
+	}
 
 	fmt.Println(nGoroutines, bufferSize, fInput, fOutput)
 }
